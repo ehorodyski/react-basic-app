d4ea543dd4d19f5b3ab616ff155b13b90cff8c98
Eric Horodyski <eric@horodyski.me>
2019-07-12 22:24:26 -0400

    Add NavBar functional component.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 //https://auth0.com/blog/react-tutorial-building-and-securing-your-first-app/
 
 import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <NavBar />
         <p>Work in progress.</p>
       </div>
     );
---
+++ frontend/src/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+// Functional (Dumb) Component
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function NavBar() {
+  return (
+    <nav className="navbar navbar-dark bg-primary fixed-top">
+      <Link className="navbar-brand" to="/">
+        Q&amp;A App
+      </Link>
+    </nav>
+  );
+}
